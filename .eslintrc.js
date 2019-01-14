77b5eb8f75e7dc06aee229c086b24ddb35f639ef
莫绍补 <shaobu.mo@weimob.com>
2019-01-14 16:09:17 +0800

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,8 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "parser":"babel-eslint", //
-    // "extends":"react-app",   //不然import，修饰器，等等慧报错，所以引入继承的规则
+    "parser":"babel-eslint", //转化一些常规eslint不能解析的代码转化成被常规转化的代码比如es6的import
+    "extends":"react-app",   //不然import，修饰器，等等慧报错，所以引入继承的规则（react-app里面的规则）
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
@@ -13,7 +13,7 @@ module.exports = {
         "sourceType": "module"
     },
     "plugins": [
-        "react"  //必须下载eslint-plugin-react插件
+        "react"  //如果使用到 必须下载eslint-plugin-react插件
     ],
     "rules": {
         "no-alert": 2,
