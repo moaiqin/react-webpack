bcf89acea10d3db8246fd31a526f91b42bb969a2
莫绍补 <shaobu.mo@weimob.com>
2019-01-14 16:41:41 +0800

    update eslint 使用react-app规则和插件的使用配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,9 +19,6 @@ module.exports = {
         // "no-alert": 2,
         // "no-console":2
         "eqeqeq":0,
-        "no-script-url":0,
-        "jsx-a11y/anchor-has-content": "off",
-        "jsx-a11y/alt-text": "off",
-        "no-empty-pattern": "off"
+        "no-script-url":0
     }
 };
